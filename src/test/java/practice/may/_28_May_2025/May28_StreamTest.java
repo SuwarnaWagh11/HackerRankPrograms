f4181ec6c6a2511b261370ab350e98d290c834e6
Suwarna_Wagh <2zo756s3g6av2j435b5zvaf26xp724eshz7svp5ae26xclxdcyva>
2025-06-02 12:08:30 +0200

    Added practical example for May 28

--- src/test/java/practice/may/_28_May_2025/May28_StreamTest.java
+++ src/test/java/practice/may/_28_May_2025/May28_StreamTest.java
@@ -2,46 +2,56 @@ package practice.may._28_May_2025;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class May28_StreamTest {
+
+    /*Expected o/p
+     [JOHN, DOE]
+   */
     @Test
     public void convertAllToUpperCase(){
         String[] names = {"john", "doe"};
         List<String> res = Arrays.stream(names).map(String::toUpperCase).collect(Collectors.toList());
         System.out.println(res);
     }
-
+    /*Expected o/p
+     1
+   */
     @Test
-    public void countNameWithLength3(){
+    public void countNameWithLengthGreaterThan3(){
         String[] names = {"Ava", "Bob", "Charlie"};
         long res = Arrays.stream(names).filter(n -> n.length() > 3).count();
         System.out.println(res);
     }
+    /*Expected o/p
+      [Anna, Liam, Zara]
+    */
     @Test
     public void sortNameAlphabetically(){
         String[] names = {"Zara", "Liam", "Anna"};
         List<String> res = Arrays.stream(names).sorted().toList();
         System.out.println(res);
     }
-
+    /*Expected o/p
+      Alice,Bob,Anna
+    */
     @Test
     public void joinNamesByComma(){
         String[] names = {"Alice", "Bob", "Anna"};
         String res = String.join(",", names);
         System.out.println(res);
     }
-
+    /*Expected o/p
+            elephant
+    */
     @Test
     public void findLongestString(){
         String[] words = {"cat", "elephant", "dog"};
         String max = "";
         int count = 0;
         for(int i =0; i < words.length; i++){
-            System.out.println(words[i].length());
             if(words[i].length() > count){
                 max = words[i];
                 count = words[i].length();
@@ -49,7 +59,9 @@ public class May28_StreamTest {
         }
         System.out.println(max);
     }
-
+    /*Expected o/p
+        elephant
+    */
     @Test
     public void findLongestStringWithStream() {
         String[] words = {"cat", "elephant", "dog"};
@@ -57,10 +69,35 @@ public class May28_StreamTest {
         String longestStr = Arrays.stream(words).max(Comparator.comparingInt(String::length)).orElse("NotFound");
         System.out.println(longestStr);
     }
-
+    /*Expected o/p
+    [apple, banana]
+    */
     @Test
     public void removeEmptyOrBlankString() {
         String[] words = {"apple", "", " ", "banana"};
-
+        List<String> res = Arrays.stream(words)
+                .filter(a -> !a.isBlank())
+                .collect(Collectors.toList());
+        System.out.println(res);
     }
+    /*Expected o/p
+    [banana, apple]
+    */
+    @Test
+    public void findDuplicateStrings() {
+        Set<String> seen = new HashSet<>();
+        String[] words = {"apple", "banana", "apple", "orange", "banana"};
+        Set<String> res = Arrays.stream(words).filter(s -> !seen.add(s)).collect(Collectors.toSet());
+        System.out.println(res);
+    }
+    /*Expected o/p
+    {2=[hi], 3=[hey], 5=[hello, world]}
+    */
+    @Test
+    public void groupStringsByTheirLength() {
+        String[] words = {"hi", "hello", "hey", "world"};
+        Map<Integer, List<String>> res = Arrays.stream(words).collect(Collectors.groupingBy(String::length));
+        System.out.println(res);
+    }
+
 }
