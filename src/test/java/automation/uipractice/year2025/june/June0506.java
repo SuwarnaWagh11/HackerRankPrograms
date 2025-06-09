695b973fc236af6dbae7a05915f52c5b4ec7b7d5
Suwarna_Wagh <2zo756s3g6av2j435b5zvaf26xp724eshz7svp5ae26xclxdcyva>
2025-06-09 11:15:48 +0200

    Added practical example for June 02nd

---
+++ src/test/java/automation/uipractice/year2025/june/June0506.java
@@ -0,0 +1,19 @@
+package automation.uipractice.year2025.june;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class June0506 {
+
+    @Test
+    public void testBrokenLinks() throws InterruptedException {
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://pkup.epam.com/main");
+        Thread.sleep(3000);
+        driver.close();
+    }
+}
